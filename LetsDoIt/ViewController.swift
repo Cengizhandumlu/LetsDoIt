c7e8de153c59f532f641772f8835d088daf9a169
Cengizhan Dumlu <cengizhandumlu.35@gmail.com>
2023-02-07 16:06:54 +0300

    first commit

--- LetsDoIt/ViewController.swift
+++ LetsDoIt/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let myUrl = Bundle.main.infoDictionary?["BACKEND_URL"] as? String
+        print(myUrl)
     }
 
 
